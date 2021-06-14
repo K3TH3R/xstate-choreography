a2dc82c0af2529c92e4ed3f2e8011d8ec9cc67a5
Kether Saturnius <kether@primacypublishing.com>
2021-06-14 18:04:38 +1000

    made cards vertical instead

--- src/components/character.vue
+++ src/components/character.vue
@@ -9,35 +9,49 @@
       overflow-hidden
       flex-grow
       bg-gray-800
+      border-gray-700 border
     "
   >
-    <div class="flex justify-between items-center w-full">
-      <div class="flex-col w-full px-4 py-4">
-        <div class="text-xl font-bold text-gray-50">
-          {{ state.context.name }}
-        </div>
-        <div class="italic text-gray-100">
-          {{ state.context.species }} - {{ state.context.gender }}
-        </div>
-        <div class="flex w-full mt-4">
-          <div class="w-3 h-3 rounded-full mr-2" :class="statusFlagClasses" />
-          <div class="text-gray-300 text-xs uppercase">
-            {{ state.context.status }}
-          </div>
-        </div>
-        <div class="flex-col w-full mt-4">
-          <div class="text-gray-500 text-xs uppercase">Last Known Location</div>
-          <div class="text-gray-100 mt-2">
-            {{ state.context.location.name }}
-          </div>
-        </div>
-      </div>
-      <div class="h-full">
+    <div class="flex-row justify-between items-center w-full">
+      <div class="relative">
         <img
           class="w-full h-full"
           :src="state.context.image"
           :alt="state.context.name"
         />
+        <div
+          class="
+            absolute
+            right-0
+            bottom-0
+            bg-gray-800
+            px-2
+            pt-2
+            pb-1
+            rounded-tl-md
+          "
+        >
+          <div class="flex w-full items-center">
+            <div class="w-3 h-3 rounded-full mr-2" :class="statusFlagClasses" />
+            <div class="text-gray-300 text-xs uppercase">
+              {{ state.context.status }}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="flex-row w-full px-4 py-4">
+        <div class="text-lg font-bold text-gray-50">
+          {{ state.context.name }}
+        </div>
+        <div class="italic text-gray-400">
+          {{ state.context.species }} - {{ state.context.gender }}
+        </div>
+        <div class="flex-col w-full mt-4">
+          <div class="text-gray-500 text-xs uppercase">Last Known Location</div>
+          <div class="text-gray-100 mt-1">
+            {{ state.context.location.name }}
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -58,11 +72,11 @@ export default {
     const { state, send } = useActor(props.characterRef)
     const statusFlagClasses = computed(() => {
       if (state.value.context.status === 'Alive') {
-        return 'bg-green-700'
+        return 'bg-green-400'
       } else if (state.value.context.status === 'Dead') {
-        return 'bg-red-700'
+        return 'bg-red-500'
       }
-      return 'bg-gray-700'
+      return 'bg-gray-500'
     })
 
     console.log(state.value.context)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      gray: colors.coolGray,
+      green: colors.lime,
+      red: colors.red,
+    },
     extend: {},
   },
   variants: {
